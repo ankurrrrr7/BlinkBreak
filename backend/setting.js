043a37f4c9bd44af5500ed3635617886a93c4154
ankurrrrr7 <ankurjha956@gmail.com>
2024-09-25 20:46:27 +0530

    Dropdown

--- backend/setting.js
+++ backend/setting.js
@@ -1,3 +1,8 @@
 document.getElementById('backToMain').addEventListener('click',()=>{
     window.location.href = '/frontend/index.html';
 });
+
+// dropdown menu
+document.getElementById('drop').addEventListener('click',()=>{
+    document.getElementById('myDropdown').classList.toggle("show");
+})
