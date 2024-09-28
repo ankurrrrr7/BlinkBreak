a3e34178bc0c8902f0dcf3d9acbd3f5c3ef796ea
ankurrrrr7 <ankurjha956@gmail.com>
2024-09-29 00:20:53 +0530

    final(1.0.0)

--- backend/main.js
+++ backend/main.js
@@ -1,57 +1,67 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const contentButton = document.getElementById("contentbutton1");
-    const stopButton = document.getElementById("mainstop");
+document.addEventListener("DOMContentLoaded", () => {
+  const contentButton = document.getElementById("contentbutton1");
+  const stopButton = document.getElementById("mainstop");
 
-    if (contentButton) {
-        contentButton.addEventListener("click", () => {
-            const inputValue = document.getElementById('input-values').querySelector('input').value;
-            const userTime = parseInt(inputValue);
+  if (contentButton) {
+    contentButton.addEventListener("click", () => {
+      const inputValue = document
+        .getElementById("input-values")
+        .querySelector("input").value;
+      const userTime = parseInt(inputValue);
 
-            if (!userTime || isNaN(userTime) || userTime <= 0) {
-                showError("Please enter a valid number of minutes");
-                return;
-            }
+      if (!userTime || isNaN(userTime) || userTime <= 0) {
+        showError("Please enter a valid number of minutes");
+        return;
+      }
 
-            chrome.runtime.sendMessage({action: 'startNotifications', time: userTime}, response => {
-                if (response.status === 'Notifications started') {
-                    showUpdate("Your reminder has been set for " + userTime + " minutes!");
-                }
-            });
-        });
-    }
+      chrome.runtime.sendMessage(
+        { action: "startNotifications", time: userTime },
+        (response) => {
+          if (response.status === "Notifications started") {
+            showUpdate(
+              "Your reminder has been set for " + userTime + " minutes!"
+            );
+          }
+        }
+      );
+    });
+  }
 
-    if (stopButton) {
-        stopButton.addEventListener('click', () => {
-            chrome.runtime.sendMessage({action: 'stopNotifications'}, response => {
-                if (response.status === 'Notifications stopped') {
-                    showUpdate("Blink reminder has been stopped.");
-                }
-            });
-        });
-    }
+  if (stopButton) {
+    stopButton.addEventListener("click", () => {
+      chrome.runtime.sendMessage(
+        { action: "stopNotifications" },
+        (response) => {
+          if (response.status === "Notifications stopped") {
+            showUpdate("Blink reminder has been stopped.");
+          }
+        }
+      );
+    });
+  }
 });
 
 function showError(message) {
-    showMessage(message, 'error-message', 'error');
+  showMessage(message, "error-message", "error");
 }
 
 function showUpdate(message) {
-    showMessage(message, 'update-message', 'update');
+  showMessage(message, "update-message", "update");
 }
 
 function showMessage(message, id, containerId) {
-    const container = document.getElementById(containerId);
-    if (!document.getElementById(id)) {
-        const messageElement = document.createElement("div");
-        messageElement.id = id;
-        messageElement.textContent = message;
-        messageElement.style.color = "red";
-        messageElement.style.marginTop = "10px";
-        messageElement.style.fontSize = "14px";
-        container.appendChild(messageElement);
+  const container = document.getElementById(containerId);
+  if (!document.getElementById(id)) {
+    const messageElement = document.createElement("div");
+    messageElement.id = id;
+    messageElement.textContent = message;
+    messageElement.style.color = "red";
+    messageElement.style.marginTop = "10px";
+    messageElement.style.fontSize = "14px";
+    container.appendChild(messageElement);
 
-        setTimeout(() => {
-            messageElement.remove();
-        }, 3000);
-    }
+    setTimeout(() => {
+      messageElement.remove();
+    }, 3000);
+  }
 }
--- backend/transalator.js
+++ backend/transalator.js
@@ -1,53 +1,53 @@
 const translator = {
-    en: {
-        content: "DON'T FORGET TO BLINK YOUR EYES AFTER",
-        contentminute: "MINUTES",
-        contentbutton1: "Remind Me!!",
-        mainstop: "Stop"
-    },
-    hindi: {
-        content: "अपनी आंखें झपकाना न भूलें",
-        contentminute: "मिनट",
-        contentbutton1: "मुझे याद दिलाएं!!",
-        mainstop: "रोकिये"
-    },
+  en: {
+    content: "DON'T FORGET TO BLINK YOUR EYES AFTER",
+    contentminute: "MINUTES",
+    contentbutton1: "Remind Me!!",
+    mainstop: "Stop",
+  },
+  hindi: {
+    content: "अपनी आंखें झपकाना न भूलें",
+    contentminute: "मिनट",
+    contentbutton1: "मुझे याद दिलाएं!!",
+    mainstop: "रोकिये",
+  },
 };
 
 // Cache DOM elements for easy access
-const content = document.getElementById('content');
-const contentMinute = document.getElementById('contentminute');
-const contentButton1 = document.getElementById('contentbutton1');
-const mainStop = document.getElementById('mainstop');
-const languageSelector = document.getElementById('myDropdown');
+const content = document.getElementById("content");
+const contentMinute = document.getElementById("contentminute");
+const contentButton1 = document.getElementById("contentbutton1");
+const mainStop = document.getElementById("mainstop");
+const languageSelector = document.getElementById("myDropdown");
 
 // Function to apply language settings
 function applyLanguage(language) {
-    content.innerText = translator[language].content;
-    contentMinute.innerText = translator[language].contentminute;
-    contentButton1.innerText = translator[language].contentbutton1;
-    mainStop.innerText = translator[language].mainstop;
+  content.innerText = translator[language].content;
+  contentMinute.innerText = translator[language].contentminute;
+  contentButton1.innerText = translator[language].contentbutton1;
+  mainStop.innerText = translator[language].mainstop;
 }
 
 // Set language in storage
 function setLanguage(language) {
-    chrome.storage.local.set({ selectedLanguage: language });
+  chrome.storage.local.set({ selectedLanguage: language });
 }
 
 // Get language from storage
 function getLanguage() {
-    chrome.storage.local.get(['selectedLanguage'], (result) => {
-        const language = result.selectedLanguage || 'en'; // Default to English
-        applyLanguage(language);
-        languageSelector.value = language; // Set the dropdown value
-    });
+  chrome.storage.local.get(["selectedLanguage"], (result) => {
+    const language = result.selectedLanguage || "en"; // Default to English
+    applyLanguage(language);
+    languageSelector.value = language; // Set the dropdown value
+  });
 }
 
 // Change language on dropdown change
-languageSelector.addEventListener('change', function(event) {
-    const selectedLanguage = event.target.value;
-    setLanguage(selectedLanguage);
-    applyLanguage(selectedLanguage);
+languageSelector.addEventListener("change", function (event) {
+  const selectedLanguage = event.target.value;
+  setLanguage(selectedLanguage);
+  applyLanguage(selectedLanguage);
 });
 
 // Load the language when the DOM is fully loaded
-document.addEventListener('DOMContentLoaded', getLanguage);
+document.addEventListener("DOMContentLoaded", getLanguage);
